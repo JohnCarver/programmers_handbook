344bd1239163e48bda9a65fc05800c70c63ac4cf
Dominik Aumayr <dominik.aumayr@gmail.com>
2012-11-16 19:18:15 +0100

    Allows same slugs

--- handbook/views.py
+++ handbook/views.py
@@ -10,7 +10,17 @@ def index(request):
     segments = request.path.split(r'/') # TODO geht nicht fuer '/'
 
     try:
-        selected_node = Node.objects.get(slug=segments[-2])
+        selected_nodes = Node.objects.filter(slug=segments[-2])
+        if len(selected_nodes) == 1:
+            selected_node = selected_nodes[0]
+        elif len(selected_nodes) == 0:
+            raise Http404
+        else: # more than one page with this slug -> search via full path
+            path = '/' + '/'.join(segments[1:])
+            for node in selected_nodes:
+                if node.get_url() == path:
+                    selected_node = node
+
         selected_content = selected_node.get_content
     except AttributeError:
         raise Http404
