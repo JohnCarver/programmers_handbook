924e2217df1b63cf5a28ecc0d0d65482f82dd2cf
Dominik Aumayr <dominik.aumayr@gmail.com>
2012-11-16 19:42:25 +0100

    Fixes settings for heroku deployment

--- programmers_handbook/settings.py
+++ programmers_handbook/settings.py
@@ -18,6 +18,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'handbook',                      # Or path to database file if using sqlite3.
+        'USER': 'handbook',                      # Not used with sqlite3.
+        'PASSWORD': 'handbook',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
     'development': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'handbook.sqlite',                      # Or path to database file if using sqlite3.
