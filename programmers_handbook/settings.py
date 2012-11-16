38eb4537e01609f44b7b23aaf3fe8bcf0cba2aff
Dominik Aumayr <dominik.aumayr@gmail.com>
2012-11-16 19:47:04 +0100

    Changes settings for heroku

--- programmers_handbook/settings.py
+++ programmers_handbook/settings.py
@@ -25,14 +25,6 @@ DATABASES = {
         'PASSWORD': 'handbook',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    },
-    'development': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'handbook.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
